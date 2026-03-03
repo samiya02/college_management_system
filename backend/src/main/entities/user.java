5299ff6985d8619186984d134e6e47534e375db7
samiya02 <shaiksamiya.in@gmail.com>
2026-03-03 11:56:23 +0530

    Add User entity with JPA annotations

---
+++ backend/src/main/entities/user.java
@@ -0,0 +1,16 @@
+package com.cms.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String password;
+}
